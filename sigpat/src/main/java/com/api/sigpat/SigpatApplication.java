eda9c2006cdfd3a8520fb067c474e5a640d5d47c
rafael <rlag@outlook.com>
2022-11-19 01:38:25 -0300

    Parametros iniciais do projeto

---
+++ sigpat/src/main/java/com/api/sigpat/SigpatApplication.java
@@ -0,0 +1,13 @@
+package com.api.sigpat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigpatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigpatApplication.class, args);
+	}
+
+}
---
+++ sigpat/src/test/java/com/api/sigpat/SigpatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.sigpat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigpatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
